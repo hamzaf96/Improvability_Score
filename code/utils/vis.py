10f4426901196b5cc908e234bd8e97714b6e7e4c
hamzaf96 <hamzafrigui96@gmail.com>
2023-10-07 01:43:45 +0200

    add data vis functions

--- code/utils/vis.py
+++ code/utils/vis.py
@@ -1,3 +1,28 @@
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+def vis_corr_map(data: pd.DataFrame):
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): _description_
+    """
+    # visualize the correlation map
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(data.corr(), annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+    plt.title("Correlation Heatmap")
+    plt.show()
+
+
+def vis_distribution_data(data: pd.DataFrame):
+    """_summary_
+
+    Args:
+        data (pd.DataFrame): _description_
+    """
+    plt.figure(figsize=(10, 8))
+    sns.displot(data)
+    plt.title("Distribution of the data")
+    plt.show()
