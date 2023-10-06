32d9df79ab983e9f9af734f6f24e135252a75186
hamzaf96 <hamzafrigui96@gmail.com>
2023-10-06 15:20:41 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def print_function(message: str = "hello"):
+    """_summary_
+
+    Args:
+        message (str, optional): _description_. Defaults to 'hello'.
+    """ """"""
+    print(message)
