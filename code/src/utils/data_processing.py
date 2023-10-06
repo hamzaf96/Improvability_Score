2103934064315e9c3f32c8bebfd225d7591602e3
hamzaf96 <hamzafrigui96@gmail.com>
2023-10-06 18:06:21 +0200

    add convert cat to ohe function

--- code/src/utils/data_processing.py
+++ code/src/utils/data_processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
 
 def remove_duplicate(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -41,13 +42,19 @@ def remove_nan_values(dataframe: pd.DataFrame) -> pd.DataFrame:
     return dataframe
 
 
-def convert_categorical_to_ohe(df: pd.DataFrame) -> pd.DataFrame:
-    """_summary_
+def convert_categorical_to_ohe(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """convert categorical data into numerical feature using One Hot Encoder
 
     Args:
-        df (pd.DataFrame): _description_
+        dataframe (pd.DataFrame): initial dataframe
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: dataframe with converted categorical into numerical
     """
-    pass
+    # convert categorical data into numerical using ohe
+    for col in dataframe.select_dtypes(include="object").columns:
+        encoder = OneHotEncoder()
+        encoded_category = encoder.fit_transform(col.reshape(-1, 1)).toarray()
+        dataframe[col] = encoded_category
+
+    return dataframe
