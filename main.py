46bb215967236cbe3068436cc37132272d9c8928
hamzaf96 <hamzafrigui96@gmail.com>
2023-10-09 23:15:49 +0200

    add streamlit front

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
 
+# Set the title and description for your app
+st.title("Simple Streamlit App")
+st.write("This is a basic Streamlit app that displays a line chart.")
 
-def print_function(message: str = "hello"):
-    """_summary_
+# Generate some random data for the line chart
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
 
-    Args:
-        message (str, optional): _description_. Defaults to 'hello'.
-    """ """"""
-    print(message)
+# Create a line chart using Matplotlib
+fig, ax = plt.subplots()
+ax.plot(x, y)
+ax.set_xlabel("X-axis")
+ax.set_ylabel("Y-axis")
+ax.set_title("Line Chart")
+
+# Display the chart in the Streamlit app
+st.pyplot(fig)
+
+# Add text or other components to your app
+st.write("You can add more content here.")
