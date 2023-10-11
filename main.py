c316af17d842c530edf75f18aa4ff53700fc4135
hamzaf96 <hamzafrigui96@gmail.com>
2023-10-11 17:24:24 +0200

    add create folder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import mplcursors
+import os
 from code.utils.vis import (
     vis_scatter_data,
 )
@@ -15,9 +16,20 @@ from code.src.improvability_score import ImprovabilityScore
 
 
 def main():
-    get_data_from_google_drive(
-        file_id="1Tr-x8-8fe081d1woq98BBXIv9UA6tboF", file_path="./data/dataset/data.csv"
-    )
+    # Define the folder path you want to check or create
+    folder_path_download_data = "./data/dataset/"
+    folder_path_save_data = "./data/processed_data/"
+    # Check if the folder exists
+    if not os.path.exists("./data/dataset/"):
+        # If it doesn't exist, create it
+        os.makedirs(folder_path_download_data)
+        print(f"Folder '{folder_path_download_data}' created.")
+        get_data_from_google_drive(
+            file_id="1Tr-x8-8fe081d1woq98BBXIv9UA6tboF",
+            file_path="./data/dataset/data.csv",
+        )
+    else:
+        print(f"Folder '{folder_path_download_data}' already exists.")
 
     print("Start Processing the Data !")
     df = pd.read_csv("./data/dataset/data.csv")
@@ -30,26 +42,23 @@ def main():
     improvability_score_list = improvability_score()
     print("Compute the Improvability Score: Done")
     df["Imrovability_Score"] = improvability_score_list
-    save_csv(file_path="./data/processed_data/processed_data.csv", data=df)
+
+    # Check if the folder exists
+    if not os.path.exists(folder_path_save_data):
+        # If it doesn't exist, create it
+        os.makedirs(folder_path_save_data)
+        print(f"Folder '{folder_path_save_data}' created.")
+        save_csv(file_path="./data/processed_data/processed_data.csv", data=df)
+    else:
+        print(f"Folder '{folder_path_save_data}' already exists.")
+
     # Set the title and description for your app
     st.title("Dashboard")
     st.write(
         "This is a the dashboard that will help choosen the student with the need to help."
     )
 
-    figure, scatter = vis_scatter_data(x=df["FinalGrade"], y=df["Imrovability_Score"])
-
-    # Create a cursor to display information on hover
-    cursor = mplcursors.cursor(scatter, hover=True)
-
-    # Function to display information when hovering
-    def on_hover(sel):
-        index = sel.target.index
-        sel.annotation.set_text(
-            f"FirstName: {index}\nFamilyName: {df['FamilyName'][index]}\n"
-        )
-
-    cursor.connect("add", on_hover)
+    figure, _ = vis_scatter_data(x=df["FinalGrade"], y=df["Imrovability_Score"])
 
     # Display the chart in the Streamlit app
     st.pyplot(figure)
